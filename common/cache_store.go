7307111ecea9c53195ac9f179638f12c161178c1
Gustavo Valentim <gustavosvalentim1@gmail.com>
2020-04-05 19:00:47 -0300

    doc: update comments in cache_store

--- common/cache_store.go
+++ common/cache_store.go
@@ -6,15 +6,19 @@ import (
 	"math/rand"
 )
 
+// CacheStoreItems stores CacheItems as a map of random integers as keys and CacheItem as value
 type CacheStoreItems map[int]CacheItem
+// CacheStoreMetas stores CacheItemMeta with the same random integer key and
+// CacheItemMeta as value
 type CacheStoreMetas map[int]CacheItemMeta
-
+// Shard stores Items, Metas and a Lock pointer
 type Shard struct {
 	Items		CacheStoreItems
 	Metas		CacheStoreMetas
 	Lock		*sync.RWMutex
 }
 
+// CacheStore contains an array of Shard
 type CacheStore struct {
 	Shards		[]Shard
 }
@@ -34,12 +38,14 @@ func (shard *Shard) remove(i int) {
 	shard.Lock.Unlock()
 }
 
+// Add a new item to a shard
 func (store *CacheStore) Add(item CacheItem, expiration int64) {
 	shard := store.Shards[len(store.Shards) - 1]
 	itemMeta := CacheItemMeta{Expires: expiration}
 	shard.add(item, itemMeta)
 }
 
+// Populate a CacheStore with an array of CacheItem
 func (store *CacheStore) Populate(items []CacheItem) {
 	now := time.Now().
 		Add(time.Second * time.Duration(15)).
@@ -50,6 +56,7 @@ func (store *CacheStore) Populate(items []CacheItem) {
 	}
 }
 
+// GetItems return all items from all shards
 func (store *CacheStore) GetItems() *[]CacheItem {
 	items := make([]CacheItem, 0)
 
@@ -62,6 +69,7 @@ func (store *CacheStore) GetItems() *[]CacheItem {
 	return &items
 }
 
+// Clean remove all expired items in CacheStore
 func (store *CacheStore) Clean(t int64) {
 	for i, v := range store.Shards {
 		for mi, m := range v.Metas {
@@ -72,10 +80,12 @@ func (store *CacheStore) Clean(t int64) {
 	}
 }
 
+// Invalidate will reset all the CacheStore
 func (store *CacheStore) Invalidate() {
 	*store = *NewStore()
 }
 
+// NewStore creates a new CacheStore
 func NewStore() *CacheStore {
 	shards := make([]Shard, 0)
 	shards = append(shards, Shard{
